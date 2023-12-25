61ec2de1b5e33be7a138aace402d9b708e7cf94e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-25 03:55:01 +0000

    xzczx

--- src/app/page.js
+++ src/app/page.js
@@ -8,13 +8,23 @@ export default function Home() {
     <div>
     <Script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.16/build/spline-viewer.js"></Script>
     <spline-viewer url="https://prod.spline.design/Bwjxj1PLZn7D8bzE/scene.splinecode"></spline-viewer>
+    <Script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.16/build/spline-viewer.js"></Script>
+    <spline-viewer url="https://prod.spline.design/Bwjxj1PLZn7D8bzE/scene.splinecode"></spline-viewer>
+    <Script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.16/build/spline-viewer.js"></Script>
+    <spline-viewer url="https://prod.spline.design/Bwjxj1PLZn7D8bzE/scene.splinecode"></spline-viewer>
+    <Script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.16/build/spline-viewer.js"></Script>
+    <spline-viewer url="https://prod.spline.design/Bwjxj1PLZn7D8bzE/scene.splinecode"></spline-viewer>
+    <Script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.16/build/spline-viewer.js"></Script>
+    <spline-viewer url="https://prod.spline.design/Bwjxj1PLZn7D8bzE/scene.splinecode"></spline-viewer>
+    <Script type="module" src="https://unpkg.com/@splinetool/viewer@1.0.16/build/spline-viewer.js"></Script>
+    <spline-viewer url="https://prod.spline.design/Bwjxj1PLZn7D8bzE/scene.splinecode"></spline-viewer>
 <div className="min-h-screen bg-gray-50 flex flex-col justify-center items-center">
       <div className="text-center">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 md:text-5xl">
           Welcome to My Art Gallery
         </h1>
         <p className="mt-4 text-lg text-gray-600">
-          Explore a collectixcvzxcvz on of captivating artworks that inspire and delight.
+          Explore a collectixcvsdfsadfasdf zxcvz on o sdfgs fgf captivasdfgs dfting artworks that inspire and delight.
         </p>
       </div>
       <div className="text-center">
@@ -22,7 +32,7 @@ export default function Home() {
           Welcome to My Art Gallery
         </h1>
         <p className="mt-4 text-lg text-gray-600">
-          Explore a collection of captivating artworks that inspire and delight.
+          Explore a collection of captisdfgsdfgvating artworks that inspire and delight.
         </p>
       </div>
       <div className="text-center">
