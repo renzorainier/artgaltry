5dee1d8e06cf1c4b2fa83baf4012e64ef96807e9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 04:47:41 +0000

    Tue, Dec 26, 2023, 4:47 AM +00:00

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -11,7 +11,7 @@ const GalleryComponent = () => {
 
       {/* Overlay with text on the left */}
       <div className="absolute left-0 right-0 top-0 bottom-0 flex items-center justify-center pointer-events-none">
-        <h1 classname="">welcome to </h1>
+        <h1 classname="text-white">welcome to </h1>
         <h1 id="title" className="text-9xl font-bold text-white">The Gallery</h1>
       </div>
     </div>
