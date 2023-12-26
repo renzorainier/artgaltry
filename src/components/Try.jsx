8759019032cffa16f60f8632a4d795fe5cf8c6ce
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 04:56:37 +0000

    Tue, Dec 26, 2023, 4:56 AM +00:00

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -1,12 +1,17 @@
 'use client'
+// src/components/GalleryComponent.js
 import React, { useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
 import gsap from 'gsap';
 
 const GalleryComponent = () => {
   useEffect(() => {
-    // GSAP animation for the text
-    gsap.from('.text-animation', { opacity: 0, duration: 1, delay: 0.5, stagger: 0.2 });
+    // GSAP typing animation
+    gsap.fromTo(
+      '#welcome-text',
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 1, delay: 1 }
+    );
   }, []);
 
   return (
@@ -16,11 +21,16 @@ const GalleryComponent = () => {
 
       {/* Overlay with text on the left */}
       <div className="absolute left-0 right-0 top-0 bottom-0 flex flex-col items-center justify-center pointer-events-none">
-        <h1 className="text-white text-2xl text-animation">Welcome to</h1>
-        <h1 id="title" className="text-9xl font-bold text-white text-animation">The Gallery</h1>
+        <h1 id="welcome-text" className="text-white text-2xl">
+          Welcome to
+        </h1>
+        <h1 id="title" className="text-9xl font-bold text-white">
+          The Gallery
+        </h1>
       </div>
     </div>
   );
 };
 
 export default GalleryComponent;
+
