a6ea19dd10deaf636c0defdaee8cf718531feb50
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 03:49:38 +0000

    Tue, Dec 26, 2023, 3:49 AM +00:00

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -1,9 +1,22 @@
-"use client"
+'use client'
 
+import React from 'react';
 import Spline from '@splinetool/react-spline';
 
-export default function App() {
+const SplineWithOverlay = ({ overlayText }) => {
   return (
-    <Spline scene="https://prod.spline.design/gIcjIbelGovm-e-k/scene.splinecode" />
+    <div className="relative">
+      <Spline scene="https://prod.spline.design/gIcjIbelGovm-e-k/scene.splinecode" />
+
+      {/* Overlay container */}
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+        {/* Overlay content */}
+        <div className="bg-black text-white p-4">
+          {overlayText && <p>{overlayText}</p>}
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default SplineWithOverlay;
