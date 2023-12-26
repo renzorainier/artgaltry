683d7310ca9ef2474b1f68136a49352daa4c59a7
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 04:44:11 +0000

    Tue, Dec 26, 2023, 4:44 AM +00:00

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -1,15 +1,18 @@
-'use client'// src/components/GalleryComponent.js
+'use client'
+// src/components/GalleryComponent.js
 import React from 'react';
 import Spline from '@splinetool/react-spline';
 
 const GalleryComponent = () => {
   return (
-    <div className="relative">
+    <div className="relative flex">
       {/* Spline component for the gallery */}
       <Spline scene="https://prod.spline.design/gIcjIbelGovm-e-k/scene.splinecode" />
 
-      {/* Overlay with text centered on the left */}
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 bottom-0 flex items-center pointer-events-none">
+      {/* Overlay with text on the left */}
+      <div className="absolute left-0 right-0 top-0 bottom-0 flex items-center justify-center pointer-events-none">
+        <h1
+        </h1>
         <h1 id="title" className="text-9xl font-bold text-white">The Gallery</h1>
       </div>
     </div>
