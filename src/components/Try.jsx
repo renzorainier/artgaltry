cdecb908b1357dcbb22a04b8b2495a072e21f103
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 04:10:27 +0000

    Tue, Dec 26, 2023, 4:10 AM +00:00

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -10,7 +10,7 @@ const GalleryComponent = () => {
 
       {/* Overlay with text */}
       <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center ">
-        <h1 className="text-4xl font-bold text-white">The Gallery</h1>
+        <h1 className="text-5xl font-bold text-white">The Gallery</h1>
       </div>
     </div>
   );
