5f601e1f5c386e6d9aa3dd173a5c773c589a4fbf
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 04:55:40 +0000

    Tue, Dec 26, 2023, 4:55 AM +00:00

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -1,9 +1,14 @@
 'use client'
-
-import React from 'react';
+import React, { useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
+import gsap from 'gsap';
 
 const GalleryComponent = () => {
+  useEffect(() => {
+    // GSAP animation for the text
+    gsap.from('.text-animation', { opacity: 0, duration: 1, delay: 0.5, stagger: 0.2 });
+  }, []);
+
   return (
     <div className="relative flex">
       {/* Spline component for the gallery */}
@@ -11,8 +16,8 @@ const GalleryComponent = () => {
 
       {/* Overlay with text on the left */}
       <div className="absolute left-0 right-0 top-0 bottom-0 flex flex-col items-center justify-center pointer-events-none">
-        <h1 className="text-white text-2xl">Welcome to</h1>
-        <h1 id="title" className="text-9xl font-bold text-white">The Gallery</h1>
+        <h1 className="text-white text-2xl text-animation">Welcome to</h1>
+        <h1 id="title" className="text-9xl font-bold text-white text-animation">The Gallery</h1>
       </div>
     </div>
   );
