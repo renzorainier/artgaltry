540a0b37f6eba27b5f8c1c2c317caa4332bd9d92
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 04:28:43 +0000

    Tue, Dec 26, 2023, 4:28 AM +00:00

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -8,8 +8,8 @@ const GalleryComponent = () => {
       {/* Spline component for the gallery */}
       <Spline scene="https://prod.spline.design/gIcjIbelGovm-e-k/scene.splinecode" />
 
-      {/* Overlay with text */}
-      <div className="absolute top-0 left-0 p-8 pointer-events-none">
+      {/* Overlay with text on the left */}
+      <div className="absolute top-0 left-0 bottom-0 flex items-center p-8 pointer-events-none">
         <h1 className="text-9xl font-bold text-white">The Gallery</h1>
       </div>
     </div>
