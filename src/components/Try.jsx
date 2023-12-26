00f4358ab6a3ebd2bab239ad7cf4474180f30233
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 03:53:04 +0000

    Tue, Dec 26, 2023, 3:53 AM +00:00

--- src/components/Try.jsx
+++ src/components/Try.jsx
@@ -1,22 +1,19 @@
 'use client'
-
 import React from 'react';
 import Spline from '@splinetool/react-spline';
 
-const SplineWithOverlay = ({ overlayText }) => {
+const GalleryComponent = () => {
   return (
     <div className="relative">
+      {/* Spline component for the gallery */}
       <Spline scene="https://prod.spline.design/gIcjIbelGovm-e-k/scene.splinecode" />
 
-      {/* Overlay container */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
-        {/* Overlay content */}
-        <div className="bg-black text-white p-4">
-          {overlayText && <p>{overlayText}</p>}
-        </div>
+      {/* Overlay with text */}
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center ">
+        <h1 className="text-4xl font-bold text-white">The Gallery</h1>
       </div>
     </div>
   );
 };
 
-export default SplineWithOverlay;
+export default GalleryComponent;
