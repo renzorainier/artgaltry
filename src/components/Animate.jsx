df63fedca63c05b056baac7e0f5f9adb22b00c7e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2023-12-26 03:38:19 +0000

    Tue, Dec 26, 2023, 3:38 AM +00:00

---
+++ src/components/Animate.jsx
@@ -0,0 +1,19 @@
+// src/components/AnimatedComponent.js
+'use client'
+import React, { useEffect } from 'react';
+import gsap from 'gsap';
+
+const AnimatedComponent = () => {
+  useEffect(() => {
+    // GSAP animations go here
+    gsap.from('.box', { opacity: 0, duration: 1, y: 50 });
+  }, []);
+
+  return (
+    <div className="box p-4 bg-blue-500 text-white">
+      This is an animated component!
+    </div>
+  );
+};
+
+export default AnimatedComponent;
